a454aa03f727a4325e95f11afbaebf8bbccee90a
ShwetaSB0726 <shwetabhilaressb558@gmail.com>
2024-11-08 16:29:35 +0530

    changes done

--- application/controllers/Candidate_profile.php
+++ application/controllers/Candidate_profile.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Candidate_profile extends CI_Controller
 {
 	public function __construct()
+    // shweta testingsssss
 	{
 		if( ! ini_get('date.timezone') )
 		{
